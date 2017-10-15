48580e5dbf1ff3beacb1d57d8b123ee26cd963d7
Nikita Shaposhnikov <nikshaposhnikov01@gmail.com>
2017-10-15 15:12:32 +0300

    Use simple adapter instead of array adapter.

--- app/src/main/java/com/example/android/prosbi/MainActivity.java
+++ app/src/main/java/com/example/android/prosbi/MainActivity.java
@@ -1,30 +1,33 @@
 package com.example.android.prosbi;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
   private static final int ACTIVITY_PRAYER_REQUEST = 1;
-  // Создаём пустой массив для хранения имен
-  final ArrayList<String> names = new ArrayList<>();
-  private ArrayAdapter<String> adapter;
+  public static final String KEY_REQUEST_FROM = "from";
+  public static final String KEY_REQUEST_WHEN = "when";
+  public static final String KEY_REQUEST = "request";
+
+  private List<Map<String, String>> requests;
+  private SimpleAdapter listAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    loadPrayerRequestData();
     createListView();
     findViewById(R.id.button_new_request).setOnClickListener(new View.OnClickListener() {
       @Override
@@ -44,11 +47,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     switch (requestCode) {
       case ACTIVITY_PRAYER_REQUEST:
-        String from = data.getStringExtra(PrayerRequestActivity.KEY_REQUEST_FROM);
-        String when = data.getStringExtra(PrayerRequestActivity.KEY_REQUEST_WHEN);
-        String request = data.getStringExtra(PrayerRequestActivity.KEY_REQUEST);
-        names.add(0, request);
-        adapter.notifyDataSetChanged();
+        String from = data.getStringExtra(KEY_REQUEST_FROM);
+        String when = data.getStringExtra(KEY_REQUEST_WHEN);
+        String request = data.getStringExtra(KEY_REQUEST);
+        // FIXME
+//        requests.add(0, request);
+//        adapter.notifyDataSetChanged();
         break;
     }
   }
@@ -57,59 +61,88 @@ public class MainActivity extends AppCompatActivity {
     // получаем экземпляр элемента ListView
     final ListView listView = (ListView) findViewById(R.id.list_view_requests);
     // Создаём адаптер ArrayAdapter, чтобы привязать массив к ListView
-    adapter = new ArrayAdapter<>(this,
-        android.R.layout.simple_list_item_1, names);
+    listAdapter = new SimpleAdapter(
+        this,
+        requests,
+        R.layout.list_item_prayer_request,
+        new String[] {
+            KEY_REQUEST_FROM,
+            KEY_REQUEST_WHEN,
+            KEY_REQUEST
+        },
+        new int[] {
+            R.id.text_view_from,
+            R.id.text_view_when,
+            R.id.text_view_request
+        });
+    listView.setAdapter(listAdapter);
+//    listView.setOnItemLongClickListener(
+//        new AdapterView.OnItemLongClickListener() {
+//          public boolean onItemLongClick(
+//              AdapterView<?> adapterView, View view, int position, long itemId) {
+//            confirmAndDeleteItem(adapterView.getItemAtPosition(position).toString());
+//            return true;
+//          }
+//        }
+//    );
+  }
 
-    listView.setOnItemLongClickListener(
-        new AdapterView.OnItemLongClickListener() {
-          public boolean onItemLongClick(
-              AdapterView<?> adapterView, View view, int position, long itemId) {
-            confirmAndDeleteItem(adapterView.getItemAtPosition(position).toString());
-            return true;
-          }
-        }
-    );
-
-    // Привяжем массив через адаптер к ListView
-    listView.setAdapter(adapter);
+  private void configureSearchEditText() {
     final EditText editText = (EditText) findViewById(R.id.edit_text_search);
     editText.setVisibility(View.GONE);
 
     // Прослушиваем нажатия клавиш
-    editText.setOnKeyListener(new View.OnKeyListener() {
-      public boolean onKey(View v, int keyCode, KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN)
-          if (keyCode == KeyEvent.KEYCODE_ENTER) {
-            names.add(0, editText.getText().toString());
-            adapter.notifyDataSetChanged();
-            editText.setText("");
-            return true;
-          }
-        return false;
-      }
-    });
+//    editText.setOnKeyListener(new View.OnKeyListener() {
+//      public boolean onKey(View v, int keyCode, KeyEvent event) {
+//        if (event.getAction() == KeyEvent.ACTION_DOWN)
+//          if (keyCode == KeyEvent.KEYCODE_ENTER) {
+//            requests.add(0, editText.getText().toString());
+//            adapter.notifyDataSetChanged();
+//            editText.setText("");
+//            return true;
+//          }
+//        return false;
+//      }
+//    });
   }
 
   private void confirmAndDeleteItem(final String requestToDelete) {
-    new AlertDialog.Builder(MainActivity.this)
-        .setMessage(getString(R.string.message_alert, adapter.getCount(), requestToDelete))
-        .setNegativeButton(getString(R.string.button_no), null)
-        .setPositiveButton(
-            getString(R.string.button_yes),
-            new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int arg1) {
-                adapter.remove(requestToDelete);
-                adapter.notifyDataSetChanged();
-              }
-            })
-        .show();
+//    new AlertDialog.Builder(MainActivity.this)
+//        .setMessage(getString(R.string.message_alert, adapter.getCount(), requestToDelete))
+//        .setNegativeButton(getString(R.string.button_no), null)
+//        .setPositiveButton(
+//            getString(R.string.button_yes),
+//            new DialogInterface.OnClickListener() {
+//              public void onClick(DialogInterface dialog, int arg1) {
+//                adapter.remove(requestToDelete);
+//                adapter.notifyDataSetChanged();
+//              }
+//            })
+//        .show();
   }
 
   private void startPrayerRequestActivity(String from, String when, String request) {
     Intent intent = new Intent(this, PrayerRequestActivity.class);
-    intent.putExtra(PrayerRequestActivity.KEY_REQUEST_FROM, from);
-    intent.putExtra(PrayerRequestActivity.KEY_REQUEST_WHEN, when);
-    intent.putExtra(PrayerRequestActivity.KEY_REQUEST, request);
+    intent.putExtra(KEY_REQUEST_FROM, from);
+    intent.putExtra(KEY_REQUEST_WHEN, when);
+    intent.putExtra(KEY_REQUEST, request);
     startActivityForResult(intent, ACTIVITY_PRAYER_REQUEST);
+  }
+
+  private void loadPrayerRequestData() {
+    requests = new ArrayList<>();
+
+    // TODO replace this test code with real loading of the requests from a JSON or a database
+    Map<String, String> itemMap = new HashMap<>();
+    itemMap.put(KEY_REQUEST_FROM, "Nikita S.");
+    itemMap.put(KEY_REQUEST_WHEN, "Last week?");
+    itemMap.put(KEY_REQUEST, "This is a sample resuest\nwith several line feeds\nand with some details.");
+    requests.add(itemMap);
+
+    itemMap = new HashMap<>();
+    itemMap.put(KEY_REQUEST_FROM, "Denys D.");
+    itemMap.put(KEY_REQUEST_WHEN, "Right now");
+    itemMap.put(KEY_REQUEST, "If you see this then your first SimpleAdaper has started to work");
+    requests.add(itemMap);
   }
 }
--- app/src/main/java/com/example/android/prosbi/PrayerRequestActivity.java
+++ app/src/main/java/com/example/android/prosbi/PrayerRequestActivity.java
@@ -6,11 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
 public class PrayerRequestActivity extends AppCompatActivity {
-
-  public static final String KEY_REQUEST_FROM = "from";
-  public static final String KEY_REQUEST_WHEN = "when";
-  public static final String KEY_REQUEST = "request";
-
   private EditText editTextFrom;
   private EditText editTextWhen;
   private EditText editTextRequest;
@@ -26,17 +21,17 @@ public class PrayerRequestActivity extends AppCompatActivity {
 
   private void configureFrom() {
     editTextFrom = (EditText) findViewById(R.id.edit_text_from);
-    editTextFrom.setText(getIntent().getStringExtra(KEY_REQUEST_FROM));
+    editTextFrom.setText(getIntent().getStringExtra(MainActivity.KEY_REQUEST_FROM));
   }
 
   private void configureWhen() {
     editTextWhen = (EditText) findViewById(R.id.edit_text_when);
-    editTextWhen.setText(getIntent().getStringExtra(KEY_REQUEST_WHEN));
+    editTextWhen.setText(getIntent().getStringExtra(MainActivity.KEY_REQUEST_WHEN));
   }
 
   private void configureRequest() {
     editTextRequest = (EditText) findViewById(R.id.edit_text_request);
-    editTextRequest.setText(getIntent().getStringExtra(KEY_REQUEST));
+    editTextRequest.setText(getIntent().getStringExtra(MainActivity.KEY_REQUEST));
   }
 
   public void onBackPressed() {
@@ -46,10 +41,9 @@ public class PrayerRequestActivity extends AppCompatActivity {
 
   private void saveToResult() {
     Intent result = new Intent();
-    result.putExtra(KEY_REQUEST_FROM, editTextFrom.getText().toString());
-    result.putExtra(KEY_REQUEST_WHEN, editTextWhen.getText().toString());
-    result.putExtra(KEY_REQUEST, editTextRequest.getText().toString());
+    result.putExtra(MainActivity.KEY_REQUEST_FROM, editTextFrom.getText().toString());
+    result.putExtra(MainActivity.KEY_REQUEST_WHEN, editTextWhen.getText().toString());
+    result.putExtra(MainActivity.KEY_REQUEST, editTextRequest.getText().toString());
     setResult(RESULT_OK, result);
   }
-
 }
